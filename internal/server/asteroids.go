92b744fc2905a0a9881ad0837fb1a07ef8630a50
briancbarrow <briancbarrow@gmail.com>
2024-05-02 16:46:49 -0600

    handle rate limits on asteroid data

--- internal/server/asteroids.go
+++ internal/server/asteroids.go
@@ -10,8 +10,14 @@ import (
 func (s *Server) AsteroidHandler(w http.ResponseWriter, r *http.Request) {
 	asteroidData, err := nasa.GetAsteroidData()
 	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		log.Fatalf("Error getting asteroid data: %v", err)
+		if err.Error() == "error making request to NASA API. Rate limit exceeded" {
+			w.Header().Set("Retry-After", "60")
+			http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
+			return
+		} else {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			log.Fatalf("Error getting asteroid data: %v", err)
+		}
 	}
 
 	jsonResp, err := json.Marshal(asteroidData)
